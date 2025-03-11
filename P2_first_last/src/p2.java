ee3cc5d42dc194680d1451abc76410fa184a18dd
1948771 <1948771@01-18129.pusd.dom>
2025-03-11 14:06:46 -0700

    Finished step one of reading

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,9 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class p2 {
 
+	private static Scanner scanner;
+	private int rows, cols, maps;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("p2");
+	    String filename = "H:\\git\\ds-project-2-Nishk04-1\\P2_first_last\\src\\TestMaze01";
+		readMap("TestMaze01");
 	}
-
+	
+	public static void readMap(String filename) {
+		try {
+			File file = new File(filename);
+			Scanner scanner = new Scanner(file);
+			
+			int numRows = scanner.nextInt();
+			int numCols = scanner.nextInt();
+			int numRooms = scanner.nextInt();
+			
+			int rowIndex = 0;
+			//process the map!
+			while(scanner.hasNextLine()) {
+				String row = scanner.nextLine();
+				
+				if(row.length()>0) {
+					for(int i = 0; i < numCols && i < row.length(); i++) {
+						char element = row.charAt(i);
+						Tile obj = new Tile(rowIndex, i, element);
+					}
+				}
+			}
+			
+		} catch (FileNotFoundException e){
+			System.out.println(e);
+		}
+	}
+	
 }
